6e036e6c8dff36b5fbf364c03c2c9b30e8016d18
Rina Motoyama <127751292+MotRina@users.noreply.github.com>
2023-12-11 15:00:04 +0900

    Update triple.hs

--- no8/triple.hs
+++ no8/triple.hs
@@ -1,12 +1,26 @@
-primes (x:xs) = x : primes (sieve x xs)
-  where sieve x xs = filter (\y -> y `mod` x /= 0) xs
-isPrime n primesList = n `elem` takeWhile (<= n) primesList
-
-main = print $ take 20 triplePrimes
-
-triplePrimes = filter isTriplePrime $ zip3 primesList (tail primesList) (drop 2 primesList)
-  where
+main = print $ take 10 $ triplePrimes primesList
+  where 
     primesList = primes [2..]
-    isTriplePrime (p, q, r) = (q == p + 2 || q == p + 4) && r == p + 6 && isPrime q primesList && isPrime r primesList
+​
+    sieve x xs = filter notDiv xs
+      where notDiv y = y `mod` x /= 0
+​
+    primes (x:xs) = x : primes (sieve x xs)
+​
+    triplePrimes (x:y:z:xs)
+      | (x + 2 == y && x + 6 == z) || (x + 4 == y && x + 6 == z) = (x, y, z) : triplePrimes (y:z:xs)
+      | otherwise = triplePrimes (y:z:xs)
+
+
+-- primes (x:xs) = x : primes (sieve x xs)
+--   where sieve x xs = filter (\y -> y `mod` x /= 0) xs
+-- isPrime n primesList = n `elem` takeWhile (<= n) primesList
+
+-- main = print $ take 20 triplePrimes
+
+-- triplePrimes = filter isTriplePrime $ zip3 primesList (tail primesList) (drop 2 primesList)
+--   where
+--     primesList = primes [2..]
+--     isTriplePrime (p, q, r) = (q == p + 2 || q == p + 4) && r == p + 6 && isPrime q primesList && isPrime r primesList
 
 
